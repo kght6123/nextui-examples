911d32e165d0a18f674c5f13370a683ebddb8ab3
kght6123 <i.feeling.song+github@gmail.com>
2024-01-14 22:55:50 +0900

    initial commit.

--- packages/examples/nextui/vite/nextui-vite-example/src/App.tsx
+++ packages/examples/nextui/vite/nextui-vite-example/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { AvatarList } from './ui/molecules/avatarList'
+import Breadcrumb from './ui/molecules/breadcrumb'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,7 +30,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <AvatarList />
+      <section className="flex flex-col">
+        <AvatarList />
+        <Breadcrumb />
+      </section>
     </>
   )
 }
---
+++ packages/examples/nextui/vite/nextui-vite-example/src/ui/molecules/breadcrumb.tsx
@@ -0,0 +1,13 @@
+import {Breadcrumbs, BreadcrumbItem} from "@nextui-org/react";
+
+export default function Breadcrumb() {
+  return (
+    <Breadcrumbs>
+      <BreadcrumbItem>Home</BreadcrumbItem>
+      <BreadcrumbItem>Music</BreadcrumbItem>
+      <BreadcrumbItem>Artist</BreadcrumbItem>
+      <BreadcrumbItem>Album</BreadcrumbItem>
+      <BreadcrumbItem>Song</BreadcrumbItem>
+    </Breadcrumbs>
+  );
+}
