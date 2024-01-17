d53f05cc591cdcd0ad50cdd668eeb0c42fecdd77
kght6123 <i.feeling.song+github@gmail.com>
2024-01-17 23:34:33 +0900

    add example.

--- packages/examples/nextui/vite/nextui-vite-example/tailwind.config.ts
+++ packages/examples/nextui/vite/nextui-vite-example/tailwind.config.ts
@@ -10,13 +10,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        // Using modern rgb
+        // 1. Using modern rgb
         // primary: 'rgb(var(--color-primary) / <alpha-value>)',
         // secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
-        // Using modern hsl
+        // 2. Using modern hsl
         // primary: 'hsl(var(--color-primary) / <alpha-value>)',
         // secondary: 'hsl(var(--color-secondary) / <alpha-value>)',
-        // Using legacy rgba
+        // 3. Using legacy rgba
         primary: 'rgba(var(--color-primary), <alpha-value>)',
         secondary: 'rgba(var(--color-secondary), <alpha-value>)',
       },
